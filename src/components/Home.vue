a8a0e273cd72c3d38823fbc815ca4f92ee60fda9
WWW-max <464711201@qq.com>
2021-05-12 10:59:46 +0800

    实现基本的主页布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,13 @@
 <template>
-  <el-button type="info" @click="logout">退出</el-button>
+  <!-- <el-button type="info" @click="logout">退出</el-button> -->
+  <el-container class="home-container">
+  <el-header>Header</el-header>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
+
 </template>
 
 <script>
@@ -14,4 +22,17 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.home-container{
+    height:100%;
+}
+.el-header{
+  background-color:#373D41;
+}
+.el-aside{
+  background-color:#333744;
+}
+.el-main{
+  background-color:#eaedf1;
+}
+
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, Form, FormItem ,Input} from 'element-ui'
+import { Button, Form, FormItem ,Input,Container,Header,Aside, Main} from 'element-ui'
 // 导入弹框提示组件
 import{Message} from 'element-ui'
 
@@ -7,4 +7,8 @@ Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
 Vue.prototype.$message = Message//全局挂载  将弹框组件挂载到了Vue的原型上
