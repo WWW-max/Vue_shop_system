87062e3011b971ca0134bfb80a15cc84498494d6
WWW-max <464711201@qq.com>
2021-05-12 13:10:51 +0800

    实现导航菜单的基本结构

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,61 +1,85 @@
 <template>
   <el-container class="home-container">
-<!-- 头部区域 -->
-  <el-header>
-    <div>
-      <img src="../assets/shop_logo.png" alt="">
-      <span>电商后台管理系统</span>
-    </div>
-  <el-button type="info" @click="logout">退出</el-button>
-  </el-header>
-  <!-- 页面主体区域 -->
-  <el-container>
-    <!-- 侧边栏 -->
-    <el-aside width="200px">Aside</el-aside>
-    <!-- 右侧内容主体 -->
-    <el-main>Main</el-main>
-  </el-container>
-</el-container>
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/shop_logo.png" alt="" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">
+        <!-- 侧边栏菜单区域 -->
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+        <!-- 一级菜单 -->
+          <el-submenu index="1">
+            <!-- 一级菜单的模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i class="el-icon-location"></i>
+              <!-- 文本 -->
+              <span>导航一</span>
+            </template>
 
+            <!-- 二级菜单 -->
+            <el-menu-item index="1-4-1">
+              <!-- 图标 -->
+              <i class="el-icon-location"></i>
+              <!-- 文本 -->
+              <span>导航一</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-   methods: {
-       logout(){
-            window.sessionStorage.clear();
-            this.$router.push("/login");
-       }   
-   }
-}
+  methods: {
+    logout() {
+      window.sessionStorage.clear();
+      this.$router.push("/login");
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
-.home-container{
-    height:100%;
+.home-container {
+  height: 100%;
 }
-.el-header{
-  background-color:#373D41;
-  display:flex;
+.el-header {
+  background-color: #373d41;
+  display: flex;
   justify-content: space-between;
-  padding-left:0;//左图标靠左贴边对齐
-  align-items: center;//退出按钮上下居中
+  padding-left: 0; //左图标靠左贴边对齐
+  align-items: center; //退出按钮上下居中
   color: #fff;
   font-size: 20px;
-  > div{ //嵌套
+  > div {
+    //嵌套
     display: flex;
-    align-items: center;//纵向居中对齐
-    span{
-      margin-left: 15px;//文本和标签有间隔
+    align-items: center; //纵向居中对齐
+    span {
+      margin-left: 15px; //文本和标签有间隔
     }
   }
-
 }
-.el-aside{
-  background-color:#333744;
+.el-aside {
+  background-color: #333744;
 }
-.el-main{
-  background-color:#eaedf1;
+.el-main {
+  background-color: #eaedf1;
 }
-
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,17 @@
 import Vue from 'vue'
-import { Button, Form, FormItem ,Input,Container,Header,Aside, Main} from 'element-ui'
+import {
+    Button,
+    Form,
+    FormItem,
+    Input,
+    Container,
+    Header,
+    Aside,
+    Main,
+    Menu,
+    Submenu,
+    MenuItem
+} from 'element-ui'
 // 导入弹框提示组件
 import{Message} from 'element-ui'
 
@@ -11,4 +23,8 @@ Vue.use(Container)
 Vue.use(Header)
 Vue.use(Aside)
 Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+
 Vue.prototype.$message = Message//全局挂载  将弹框组件挂载到了Vue的原型上
