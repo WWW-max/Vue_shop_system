9817a03c31f2d113d6bbff172381720c0d2188c7
WWW-max <464711201@qq.com>
2021-05-13 19:08:22 +0800

    通过路由的形式展示用户列表组件

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
 import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -12,7 +13,10 @@ const routes = [
     path: '/home',
     component: Home, 
     redirect: '/welcome',
-    children: [{ path: '/welcome', component: Welcome}]
+    children: [
+      { path: '/welcome', component: Welcome},
+      {path:'/users',component: Users}
+    ]
   }
 ]
 
