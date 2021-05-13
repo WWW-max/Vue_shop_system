08e392dc4901f8ccad59b3b93b9f48749991fc26
WWW-max <464711201@qq.com>
2021-05-13 09:27:23 +0800

    实现首页路由的重定向效果

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,18 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/', redirect: '/login'},
-  { path: '/login', component: Login},
-  { path: '/home', component: Home}
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
+  {
+    path: '/home',
+    component: Home, 
+    redirect: '/welcome',
+    children: [{ path: '/welcome', component: Welcome}]
+  }
 ]
 
 const router = new VueRouter({
@@ -15,16 +21,16 @@ const router = new VueRouter({
 })
 
 //挂载路由导航守卫
-router.beforeEach((to,from,next)=>{
+router.beforeEach((to, from, next) => {
   //to 将要访问的路径
   //from 代表从哪个路径跳转而来
   //next 是一个函数，表示放行
   //next()放行  next('/login') 强制跳转
 
-  if(to.path==='/login') return next();
+  if (to.path === '/login') return next();
   //获取token
   const tokenStr = window.sessionStorage.getItem('token');
-  if(!tokenStr) return next('/login')//没有token强制跳转
+  if (!tokenStr) return next('/login')//没有token强制跳转
   next()  //放行
 })
 export default router
