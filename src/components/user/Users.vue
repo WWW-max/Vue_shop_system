620867a9e0c9b58ac70c4bcee21b5716ebef446e
WWW-max <464711201@qq.com>
2021-05-15 18:58:24 +0800

    修改用户状态

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -33,7 +33,7 @@
         <el-table-column label="状态">
           <!-- 使用作用域插槽接收scop就可以用scop点出row -->
           <template slot-scope="scope">
-            <el-switch v-model="scope.row.mg_state"> </el-switch>
+            <el-switch v-model="scope.row.mg_state" @change="userStateChange(scope.row)"> </el-switch>
           </template>
         </el-table-column>
         <el-table-column label="操作" width="180px">
@@ -109,6 +109,17 @@ export default {
       // console.log(newPage);
       this.queryInfo.pagenum=newPage
       this.getUserList()
+    },
+    //监听switch开关状态的改变
+    async userStateChange(userinfo){
+        console.log(userinfo)
+         const {data: res} = await this.$http.put(
+           `users/${userinfo.id}/state/${userinfo.mg_state}`)
+        if(res.meta.status !==200){
+          userinfo.mg_state = !userinfo.mg_state
+          return this.$message.error('更新用户状态失败！')
+        }
+        this.$message.success('更新用户状态成功！')
     }
   },
 };
