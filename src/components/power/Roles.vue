bd7ab601377442eca75f3c02e7bcd27edf06ddc2
WWW-max <464711201@qq.com>
2021-10-26 00:37:10 +0800

    通过路由展示角色列表组件

---
+++ src/components/power/Roles.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图 -->
+    <el-card>
+        <!-- 添加角色按钮区域 -->
+        <el-row>
+            <el-col>
+                <el-button type="primary">添加角色</el-button>
+            </el-col>
+        </el-row>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+      return{
+      //所有角色列表数据
+      rolelist:[]
+      }
+  },
+  created(){
+      this.getRolesList();
+  },
+  methods:{
+      //获取所有角色列表
+     async getRolesList(){
+         const {data:res} = await this.$http.get('roles')
+
+         if(res.meta.status!==200){
+             return this.$message.error('获取角色列表失败！')
+         }
+
+         this.rolelist = res.data
+
+         console.log(this.rolelist);
+         
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
