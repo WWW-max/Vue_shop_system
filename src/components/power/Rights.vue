5e35bb8d2cc7af377f710b7ea1ad49ffc26eddeb
WWW-max <464711201@qq.com>
2021-10-25 22:54:54 +0800

    绘制面包屑导航和卡片视图

--- src/components/power/Rights.vue
+++ src/components/power/Rights.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
-        <h3>权限列表组件页面</h3>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+    </el-breadcrumb>
+
+       <!--卡片视图  -->
+    <el-card>123 </el-card>
     </div>
 </template>
 
