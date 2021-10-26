2d600a1a7547d73a37bef81191572c7e65a9e616
WWW-max <464711201@qq.com>
2021-10-26 11:47:29 +0800

    渲染角色列表数据

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -15,6 +15,23 @@
                 <el-button type="primary">添加角色</el-button>
             </el-col>
         </el-row>
+
+        <!-- 角色列表区域 -->
+        <el-table :data="rolelist" border stripe>
+          <!-- 展开列 -->
+          <el-table-column type="expand"></el-table-column>
+          <!-- 索引列 -->
+          <el-table-column type="index"></el-table-column>
+          <el-table-column label="角色名称" prop="roleName"></el-table-column>
+          <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
+          <el-table-column label="操作" width="300px">
+            <template>
+              <el-button size="mini" type="primary" icon="el-icon-edit">编辑</el-button>
+              <el-button size="mini" type="danger" icon="el-icon-delete">删除</el-button>
+              <el-button size="mini" type="warning" icon="el-icon-setting">分配权限</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
     </el-card>
   </div>
 </template>
