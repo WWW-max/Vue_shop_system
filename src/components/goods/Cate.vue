ea5fd26f8e67ca72f97c1073d7b588c6740fafc1
WWW-max <464711201@qq.com>
2021-11-02 21:50:19 +0800

    实现分页功能

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -11,12 +11,12 @@
     <el-card>
         <el-row>
             <el-col>
-                <el-button type="primary">添加分类</el-button>
+                <el-button type="primary" @click="showAddCateDialog">添加分类</el-button>
             </el-col>
         </el-row>
  
-        <!-- 表格 -->
-        <tree-table :data="catelist" :columns="columns" 
+        <!-- 表格 -->   
+        <tree-table class="treeTable" :data="catelist" :columns="columns" 
         :selection-type="false"
         :expand-type="false"
         show-index index-text="#"
@@ -37,86 +37,127 @@
             </template>
 
              <!-- 操作 -->
-            <template slot="opt" slot-scope="scope">
+            <template slot="opt" >
                <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
                <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button></el-button>
             </template>
          </tree-table>
 
-
-
-        <!-- 分页区域 -->
+          <!-- 分页区域 -->
+        <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="querInfo.pagenum"
+        :page-sizes="[3, 5, 10, 15]"
+        :page-size="querInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+        </el-pagination> 
     </el-card>
 
+    <!-- 添加分类的对话框 -->
+    <el-dialog
+    title="添加分类"
+    :visible.sync="addCateDialogVisible"
+    width="30%"
+    >
+    <span>这是一段信息</span>
+    <span slot="footer" class="dialog-footer">
+        <el-button @click="addCateDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCateDialogVisible = false">确 定</el-button>
+    </span>
+   </el-dialog>
   </div>
 </template>
 
 <script>
 export default {
-   data(){
-       return {
-           //查询条件
-           queryInfo:{
-               type:3,
-               pagenum:1,
-               pagesize:5
-           },
-           //商品分类数据，默认为空
-           catelist:[],
-           //总数据条数
-           total:0,
-           //为table指定列的定义
-           columns:[
-               {
-               label:'分类名称',
-               prop:'cat_name'
-               },{
-                   label:'是否有效',
-                   //表示将当前列定义为模板列
-                   type:'template',
-                   //表示当前这一列使用模板名称
-                   template:'isok'
-               },{
-                   label:'排序',
-                   //表示将当前列定义为模板列
-                   type:'template',
-                   //表示当前这一列使用模板名称
-                   template:'order'
-               },{
-                   label:'操作',
-                   //表示将当前列定义为模板列
-                   type:'template',
-                   //表示当前这一列使用模板名称
-                   template:'opt'
-               }
-           ]
-       }
-   },
-   created(){
-       //调用方法获取商品分类数据
-       this.getCateList()
-   },
-   methods:{
-       //获取商品分类数据
-       async getCateList(){
-         const {data:res} = await this.$http.get('categories',{params:this.querInfo})
+  data() {
+    return {
+      //查询条件
+      querInfo: {
+        type: 3,
+        pagenum: 1,
+        pagesize: 5,
+      },
+      //商品分类数据，默认为空
+      catelist: [],
+      //总数据条数
+      total: 0,
+      //为table指定列的定义
+      columns: [
+        {
+          label: "分类名称",
+          prop: "cat_name",
+        },
+        {
+          label: "是否有效",
+          //表示将当前列定义为模板列
+          type: "template",
+          //表示当前这一列使用模板名称
+          template: "isok",
+        },
+        {
+          label: "排序",
+          //表示将当前列定义为模板列
+          type: "template",
+          //表示当前这一列使用模板名称
+          template: "order",
+        },
+        {
+          label: "操作",
+          //表示将当前列定义为模板列
+          type: "template",
+          //表示当前这一列使用模板名称
+          template: "opt",
+        },
+      ],
+      //控制添加分类对话框的显示与隐藏
+      addCateDialogVisible:false
+    
+    };
+  },
+  created() {
+    //调用方法获取商品分类数据
+    this.getCateList();
+  },
+  methods: {
+    //获取商品分类数据
+    async getCateList() {
+      const { data: res } = await this.$http.get("categories", {
+        params: this.querInfo,
+      });
 
-         if(res.meta.status !==200){
-             return this.$message.error('获取商品分类失败！')
-         }
+      if (res.meta.status !== 200) {
+        return this.$message.error("获取商品分类失败！");
+      }
 
-        //把数据列表赋值给catelist
-         this.catelist = res.data
-         console.log('和咯',this.catelist)
-        //  为总数据条数赋值
-        this.total =  res.data.total;
-         
-       }
-   }
-}
+      //把数据列表赋值给catelist
+      this.catelist = res.data.result;
+      //  为总数据条数赋值
+      this.total = res.data.total;
+    },
+    //监听pagesize改变
+    handleSizeChange(newSize) {
+      this.querInfo.pagesize = newSize;
+      this.getCateList();
+    },
+    //监听pagenum改变
+    handleCurrentChange(newPage) {
+      this.querInfo.pagenum = newPage;
+      this.getCateList();
+    },
+    //点击按钮，展示添加分类的对话框
+    showAddCateDialog(){
+        addCateDialogVisible = true;
+    }
+  },
+};
 </script>
 
 
 <style lang="less" scoped>
-
+.treeTable {
+  margin-top: 15px;
+}
 </style>>
