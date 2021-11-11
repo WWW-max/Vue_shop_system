f2225f0054be32b0792fca3a16fd5c11dba607e3
WWW-max <464711201@qq.com>
2021-11-11 21:23:40 +0800

    通过路由加载商品列表组件

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,8 @@ import Users from '../components/user/Users.vue'
 import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
 import Cate from '../components/goods/Cate.vue'
-import Params from '../components/goods/Params'
+import Params from '../components/goods/Params.vue'
+import GoodsList from '../components/goods/List.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -25,7 +26,8 @@ const routes = [
       { path: '/rights',component:Rights},
       { path: '/roles',component:Roles},
       { path: '/categories',component:Cate},
-      { path: '/params',component:Params}
+      { path: '/params',component:Params},
+      { path: '/goods',component:GoodsList},
     ]
   }
 ]
