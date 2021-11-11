91b5a6c29adf9cdff2d74f4616daa33ce463bf5f
WWW-max <464711201@qq.com>
2021-11-11 22:34:40 +0800

    渲染商品表格数据

--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -19,6 +19,22 @@
               <el-button type="primary">添加商品</el-button>
          </el-col>
      </el-row>
+
+     <!-- table表格区域 -->
+     <el-table :data="goodslist" border stripe>
+         <el-table-column type="index"></el-table-column>
+         <el-table-column label="商品名称" prop="goods_name"></el-table-column>
+         <el-table-column label="商品价格(元)" prop="goods_price" width="95px"></el-table-column>
+         <el-table-column label="商品重量" prop="goods_weight" width="70px"></el-table-column>
+         <el-table-column label="创建时间" prop="add_time" width="140px"></el-table-column>
+         <el-table-column label="操作" width="130px">
+             <template slot-scope>
+                <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+                <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+             </template>
+         </el-table-column>
+
+     </el-table>
     </el-card>
   </div>
 </template>
