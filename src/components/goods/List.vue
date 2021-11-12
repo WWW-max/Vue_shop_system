67a2666546ea4ab8873295ae5334961f3ac055e8
WWW-max <464711201@qq.com>
2021-11-12 23:00:32 +0800

    实现商品列表的分页功能

--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -39,6 +39,17 @@
          </el-table-column>
 
      </el-table>
+
+     <!-- 分页区域 -->
+     <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="queryInfo.pagenum"
+      :page-sizes="[5, 10, 15, 20]"
+      :page-size="queryInfo.pagesize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total" background>
+    </el-pagination>
     </el-card>
   </div>
 </template>
@@ -75,6 +86,14 @@ export default {
            console.log(res.data)
            this.goodslist = res.data.goods;
            this.total = res.data.total;
+        },
+        handleSizeChange(newSize){
+           this.queryInfo.pagesize = newSize
+           this.getGoodsList()
+        },
+        handleCurrentChange(newPage){
+            this.queryInfo.pagenum = newPage
+            this.getGoodsList()
         }
 
         
