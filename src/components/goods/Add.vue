8ef730844e54c83b569d4b3a9388c0c45169831e
WWW-max <464711201@qq.com>
2021-11-16 23:09:15 +0800

    美化步骤条组件

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -10,13 +10,16 @@
     <!-- 卡片视图区域 -->
     <el-card>
       <!-- 提示区域 -->
-      <el-alert title="添加商品信息" type="info" show-icon :closable="false">
+      <el-alert title="添加商品信息" type="info" center show-icon :closable="false">
       </el-alert>
       <!-- 步骤条区域 -->
-      <el-steps :space="200" :active="1" finish-status="success">
-        <el-step title="已完成"></el-step>
-        <el-step title="进行中"></el-step>
-        <el-step title="步骤 3"></el-step>
+      <el-steps  :active="activeIndex" finish-status="success" align-center>
+        <el-step title="基本信息"></el-step>
+        <el-step title="商品参数"></el-step>
+        <el-step title="商品属性"></el-step>
+        <el-step title="商品图片"></el-step>
+        <el-step title="商品内容"></el-step>
+        <el-step title="完成"></el-step>
       </el-steps>
     </el-card>
   </div>
@@ -25,7 +28,9 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      activeIndex:0
+    };
   },
   created() {},
   methods: {},
