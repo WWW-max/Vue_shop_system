3995036964f86ad8487b481966411b2b31722591
WWW-max <464711201@qq.com>
2021-11-16 23:24:40 +0800

    实现步骤条和tab栏的数据联动效果

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -19,7 +19,7 @@
       >
       </el-alert>
       <!-- 步骤条区域 -->
-      <el-steps :active="activeIndex" finish-status="success" align-center>
+      <el-steps :active="activeIndex - 0" finish-status="success" align-center>
         <el-step title="基本信息"></el-step>
         <el-step title="商品参数"></el-step>
         <el-step title="商品属性"></el-step>
@@ -29,12 +29,12 @@
       </el-steps>
 
       <!-- tab栏区域 -->
-      <el-tabs :tab-position="'left'" style="height: 200px">
-        <el-tab-pane label="基本信息">基本信息</el-tab-pane>
-        <el-tab-pane label="商品参数">商品参数</el-tab-pane>
-        <el-tab-pane label="商品属性">商品属性</el-tab-pane>
-        <el-tab-pane label="商品图片">商品图片</el-tab-pane>
-        <el-tab-pane label="商品内容">商品内容</el-tab-pane>
+      <el-tabs v-model="activeIndex" :tab-position="'left'" style="height: 200px">
+        <el-tab-pane label="基本信息" name="0">基本信息</el-tab-pane>
+        <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
+        <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
+        <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
+        <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
       </el-tabs>
     </el-card>
   </div>
@@ -44,7 +44,7 @@
 export default {
   data() {
     return {
-      activeIndex: 0,
+      activeIndex: '0',
     };
   },
   created() {},
