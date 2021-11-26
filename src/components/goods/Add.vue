e639fdff1b02ed55b809408ed1d0d1e23c26df28
WWW-max <464711201@qq.com>
2021-11-26 22:13:57 +0800

    安装并配置vue-quill-editor

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -87,7 +87,12 @@
               <el-button size="small" type="primary">点击上传</el-button>
             </el-upload>
           </el-tab-pane>
-          <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
+          <el-tab-pane label="商品内容" name="4">
+            <!-- 富文本编辑器组件 -->
+            <quill-editor v-model="addForm.goods_introduce"></quill-editor>
+            <!-- 添加商品的按钮 -->
+            <el-button type="primary" class="btnAdd">添加商品</el-button>
+          </el-tab-pane>
         </el-tabs>
       </el-form>
     </el-card>
@@ -117,6 +122,8 @@ export default {
         goods_cat: [],
         //图片的数组
         pics:[],
+        //商品的详情描述
+        goods_introduce:''
       },
       addFormRules: {
         goods_name: [
@@ -282,5 +289,8 @@ export default {
 }
 .previewImg{
   width:100%;
+}
+.btnAdd{
+  margin-top:15px;
 }
 </style>>
--- src/main.js
+++ src/main.js
@@ -9,6 +9,13 @@ import './assets/css/global.css'
 
 import TreeTable from 'vue-table-with-tree-grid'
 
+//导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+//导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
 // 导入axios
 import axios from 'axios'
 
@@ -31,6 +38,8 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.component('tree-table', TreeTable)
+//将富文本编辑器，注册为全局可用的组件
+Vue.use(VueQuillEditor)
 
 //全局定义一个时间格式过滤器
 Vue.filter('dateFormat',function(originVal){
