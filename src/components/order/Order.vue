263c1cba6928c31af051ed422080b1af1e74d571
WWW-max <464711201@qq.com>
2021-11-28 20:55:57 +0800

    通过路由渲染订单列表页面

---
+++ src/components/order/Order.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+         <!-- 面包屑导航区域 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>订单管理</el-breadcrumb-item>
+            <el-breadcrumb-item>订单列表</el-breadcrumb-item>
+        </el-breadcrumb>
+
+        <!-- 卡片视图区域 -->
+        <el-card>
+            <el-row>
+                <el-col :span="8">
+                    <el-input placeholder="请输入内容">
+                        <el-button slot="append" icon="el-icon-search"></el-button>
+                    </el-input>
+                </el-col>
+            </el-row>
+        </el-card>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    },
+    created(){},
+    methods:{},
+
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
 import GoodsList from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
+import Order from '../components/order/Order.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -30,6 +31,7 @@ const routes = [
       { path: '/params',component:Params},
       { path: '/goods',component:GoodsList},
       { path: '/goods/add',component:Add},
+      { path: '/orders',component:Order},
       
     ]
   }
