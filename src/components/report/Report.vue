2cd3e93f318110aaec1e2f69a86a0054bc217e95
WWW-max <464711201@qq.com>
2021-12-01 23:45:40 +0800

    安装Echarts并渲染Demo图表

--- src/components/report/Report.vue
+++ src/components/report/Report.vue
@@ -9,16 +9,49 @@
 
     <!-- 卡片视图区域 -->
     <el-card>
-        123
+      <!-- 2.为 ECharts 准备一个定义了宽高的 DOM -->
+      <div id="main" style="width: 600px; height: 400px"></div>
     </el-card>
   </div>
 </template>
 
 <script>
+// 1.导入echarts
+import * as echarts from "echarts";
 export default {
   data() {},
   created() {},
-  methods:{},
+//   此时页面中的元素已经被渲染完毕了
+  mounted() {
+    // 3.基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById("main"));
+
+     // 4.指定图表的配置项和数据
+      var option = {
+        title: {
+          text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        legend: {
+          data: ['销量']
+        },
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [
+          {
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      };
+
+      // 5.展示数据，使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
+  },
+  methods: {},
 };
 </script>
 
